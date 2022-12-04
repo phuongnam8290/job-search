e0a4b428c5badd5dadfdb41c39864b13281a93b0
phuongnam8290 <phuongnam8290@gmail.com>
2022-12-04 10:28:02 +0700

    feat(button): Add custom button type

---
+++ tests/unit/components/ActionButton.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/vue";
+
+import ActionButton from "@/components/ActionButton.vue";
+
+describe("ActionButton", () => {
+  beforeEach(() => {
+    render(ActionButton, {
+      props: {
+        text: "Click me",
+        type: "primary",
+      },
+    });
+  });
+
+  it("render text", () => {
+    const button = screen.getByRole("button", {
+      name: /click me/i,
+    });
+    expect(button).toBeInTheDocument();
+  });
+
+  it("applies one of several styles to button", () => {
+    const button = screen.getByRole("button", {
+      name: /click me/i,
+    });
+    expect(button).toHaveClass("primary");
+  });
+});
