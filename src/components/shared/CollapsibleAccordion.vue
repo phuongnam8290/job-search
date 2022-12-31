5cd9470358ea04c4bc9c7779b1c1dffa432b1a61
phuongnam8290 <phuongnam8290@gmail.com>
2022-12-31 09:52:18 +0700

    refactor(shared): Update Accordion & Button to use Composition API

--- src/components/shared/CollapsibleAccordion.vue
+++ src/components/shared/CollapsibleAccordion.vue
@@ -1,44 +1,39 @@
 <template>
   <div class="border-b border-solid border-brand-gray-2 py-5">
-    <div class="flex cursor-pointer flex-wrap items-center justify-between" role="button" @click="toggle">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      role="button"
+      @click="toggle"
+    >
       <h3 class="text-base font-semibold"> {{ header }} </h3>
-      <font-awesome-icon :icon="carretIcon" />
+      <font-awesome-icon :icon="caretIcon" />
     </div>
 
-    <div v-if="isOpen" class="mt-5 w-full">
+    <div
+      v-if="isOpen"
+      class="mt-5 w-full"
+    >
       <slot>No data</slot>
     </div>
   </div>
 </template>
 
-<script>
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+<script setup>
+import { computed, ref } from "vue";
 
-export default {
-  name: "CollapsibleAccordion",
-  components: { FontAwesomeIcon },
-  props: {
-    header: {
-      type: String,
-      required: true,
-    },
+defineProps({
+  header: {
+    type: String,
+    required: true,
   },
-  data() {
-    return {
-      isOpen: false,
-    };
-  },
-  computed: {
-    carretIcon() {
-      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
-    },
-  },
-  methods: {
-    toggle() {
-      this.isOpen = !this.isOpen;
-    },
-  },
+});
+
+const isOpen = ref(false);
+const toggle = () => {
+  isOpen.value = !isOpen.value;
 };
+
+const caretIcon = computed(() => (isOpen.value ? ["fas", "angle-up"] : ["fas", "angle-down"]));
 </script>
 
 <style scoped></style>
